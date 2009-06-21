02e0185bd2cec7fc3f1ae1ffe71a6ffe3bd446cd
sevk <sevkme@gmail.com>
2009-06-21 19:00:06 +0800

    忘了还有个 color.rb 文件..

---
+++ color.rb
@@ -0,0 +1,36 @@
+#this file is from EE
+#
+$black="\e[30m"; $red="\e[31m"; $green="\e[32m"; 
+$yellow="\e[33m";
+$blue="\e[34m"; $pink="\e[35m"; $cyan="\e[36m"; 
+$white="\e[37m";
+
+$normal="\e[0m"; $bold="\e[1m"; $underline="\e[4m"
+$reverse="\e[7m";
+$delete="\e[9m";
+
+$b_black="\e[40m"; $b_red="\e[41m"; $b_green="\e[42m";
+$b_yellow="\e[43m";
+$b_blue="\e[44m"; $b_pink="\e[45m"; $b_cyan="\e[46m"; 
+$b_white="\e[47m";
+
+#windows 下面可以安装 msys
+class String
+  def red
+    "\e[31m#{self}#$normal"
+  end
+  def green
+    "\e[32m#{self}#$normal"
+  end
+  def yellow
+    "\e[33m#{self}#$normal"
+  end
+
+  def redb
+    "\e[41m#{self}#$normal"
+  end
+  def blueb
+    "\e[44m#{self}#$normal"
+  end
+    
+end
