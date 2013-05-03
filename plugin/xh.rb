6ccbd7fd90b734fa592b9d81021a731cb1e1e2ee
kk <sevkme@gmail.com>
2013-05-03 17:12:18 +0800

    add 笑话.rb

---
+++ plugin/xh.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+# 取笑话
+
+require 'mechanize'
+require 'htmlentities'
+require 'utf.rb'
+def joke(n=nil)
+  n ||= rand(17786)
+  url="http://xiaohua.zol.com.cn/detail1/#{n}.html"
+  a=Mechanize.new
+  s = a.get_file url
+  begin
+    s=s.match(/<div class="lC">(.*?)<div class="lastVote">/im)[1]
+  rescue
+    log ''
+    return "empty err. try again. " + joke
+  end
+  s.gsub!(/<.+?>/,' ')
+  s=s.code_a2b(guess(s) ,'utf-8')
+  "id:#{n} #{s.unescapeHTML}"
+end
+
