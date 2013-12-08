883d5bf708093a510efb854c8c9615a56562bfbd
kk <sevkme@gmail.com>
2013-12-08 11:11:08 +0800

    使用 Dir.glob

--- lib/plugin.rb
+++ lib/plugin.rb
@@ -1,31 +1,31 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 
-require 'find'
-include Find
-
 #载入plugin
 
-if File.directory? 'plugin'
-  Find.find(Dir.pwd + '/plugin/') do |e|
-    if File.extname(e) == '.rb'
-      puts 'load plugin: ' + e.to_s.red
-      load e
-    end
+a = Dir.glob 'plugin/*.rb'
+a.each { |e|
+  next if e =~ /^_/
+  begin
+    load e
+  rescue
+    log ''
+    puts 'err: plugin: ' + e.to_s.red
+    next
   end
-end
+  puts 'load plugin: ' + e.to_s.green
+}
 
 # +q
 def get_baned
   re = []
-  Find.find('.') do |f|
-    if f =~ /_baned\.ban/
-      puts ' baned file : ' + f.to_s
-      s = File.read(f)
-      p s
-      re << s
-      File.delete f
-    end
+  a = Dir.glob '*_baned.ban'
+  a.each do |f|
+    puts ' baned file : ' + f.to_s
+    s = File.read(f)
+    p s
+    re << s
+    File.delete f
   end
   re
 end
