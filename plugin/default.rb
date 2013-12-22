8045e47e7c9f5ff65effca1ed18f6efe2074f775
kk <sevkme@gmail.com>
2013-12-22 10:29:30 +0800

    按时间判定最新的帖子

--- plugin/default.rb
+++ plugin/default.rb
@@ -3,25 +3,7 @@
 
 Thread.new do
   sleep 10
-  require 'dic.rb' unless defined? getGoogle
   puts '延时加载某些函数.'
-  def method_missing(m, *args, &block)
-    sleep 0.0001
-    case s= m.to_s
-    when /^to_/
-      p caller[0] if $DEBUG
-      return super
-    when /^gsub|^empty/
-      return super
-    when /笑话/
-      return joke
-    end
-    print "方法未找到: "
-    p m
-    p args
-    #p block
-    p caller
-    #getGoogle(s)
-  end  
+
 end
 
