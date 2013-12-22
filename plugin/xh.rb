8e25fbc843b4c3e08116d7cb439aba50a0117241
kk <sevkme@gmail.com>
2013-12-22 13:31:46 +0800

    取笑话标点替换

--- plugin/xh.rb
+++ plugin/xh.rb
@@ -14,16 +14,29 @@ def joke(n=nil)
   puts url
   a=Mechanize.new
   s = a.get url
-  ti = s.at('.article-title').text
-  text = s.at('.article-text').text
+  begin
+    ti = s.at('.article-title').text
+    text = s.at('.article-text').text
+  rescue
+    log ''
+    return joke
+  end
   s= "#{ti} : #{text} "
   s=s.code_a2b(guess(s) ,'utf-8').unescapeHTML
+  s.force_encoding 'utf-8'
+
+  #中文 => 英文
+  {'：' => ':' , '，' => "," ,  /”|“/ => '"' , /[‘’]/ => "'" ,
+    /\s+|\t/ => ' ' , '？' => '?'
+  } .each { |x,y| 
+    s.gsub!(x,y)
+  }
+
   $fun ||= 800
   if s.bytesize > $fun
     sleep 0.1
     s=joke
   end
-  s.gsub!(/\s+|\t/,' ')
   s
 end
 alias 给大爷讲个笑话 joke
